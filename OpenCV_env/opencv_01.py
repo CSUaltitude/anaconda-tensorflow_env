82cdc27658f2e374406861e1d153ac68aaa6b473
CSUaltitue <703362086@qq.com>
2018-10-08 23:26:35 +0800

    opecv dev env

---
+++ OpenCV_env/opencv_01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  8 21:30:20 2018
+
+@author: zhouwei
+"""
+
+import cv2
+print(cv2._version_)
+
+
+#OPENCV 使用 ANACONDA NEVIGATOR 安装 
+#并更新了 devC++ 版本
+
+
+
